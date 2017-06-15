687094de4de61bd40601cfbb5244491da7464138
PeterCheng1 <1284995325@qq.com>
2017-06-15 15:44:36 +0800

    cnode-init-project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <c-footer></c-footer>
+  </div>
+</template>
+
+<script>
+import footer from './components/footer/footer'
+
+export default {
+  name: 'app',
+  components:{
+    'c-footer':footer
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ src/components/footer/footer.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="footer">
+        <md-bottom-bar class="barItem-wrapper" >
+            <md-bottom-bar-item  class="wrapper" v-for="item of list">
+                <router-link :to="item.path">    
+                    <i class="iconfont" :class="[ item.icon, {'active' : item.path == $route.path }]" ></i>
+                    <p :class="{'active' : item.path == $route.path }">{{ item.title }}</p>
+                    <div class="count" v-if="item.path == '/my/messages ' && count > 0">{{ count }}</div>
+                </router-link>
+            </md-bottom-bar-item>
+        </md-bottom-bar>    
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            count:0,
+            list:[
+               {
+                   title:'首页',
+                   path: '/',
+                   icon:'icon-shouye'
+               },
+               {
+                   title:'发表',
+                   path:'/topic/create',
+                   icon: 'icon-17'
+               },
+               {
+                  title: '消息',
+                  path: '/my/messages',
+                  icon:'icon-xiaoxi'
+               },
+               {
+                    title: '我的',
+                    path: '/my/home',
+                    icon: 'icon-iconfontguanyuwo'
+               }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .footer{
+        position:fixed;
+        bottom:0;
+        left:0;
+        width:100%;
+        z-index:50;
+        text-align:center;
+        .barItem-wrapper{
+            .wrapper{
+                a{
+                    text-decoration:none;
+                    vertical-align:middle;
+                    margin-top:5px;
+                   i{
+                        font-size:24px;
+                        width: 24px;
+                        min-width: 24px;
+                        height: 24px;
+                        margin: 0 auto;
+                        color:#757575;
+                        font-weight:bold;
+                        transition: all 0.3s;
+                        &.active{
+                            color:#2196f3;
+                        }
+                    }
+                    p{
+                        font-size:18px;
+                        color:#757575;
+                        margin-top:5px;
+                        &.active{
+                            color:#2196f3;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+</style>
+
